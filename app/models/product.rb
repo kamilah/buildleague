ce23a786cece56ea8b43a3be0cb162c344575fdb
kamilah <milahkesi@gmail.com>
2014-10-24 19:57:23 +0000

    forgot comma in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   else
    has_attached_file :image, :styles => { :medium => "300x", :thumb => "200x200>" }, :default_url => "default.jpg",
                      :storage => :dropbox,
-                     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                      :path => ":style/:id_:filename"
   end 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
