73ea477dd3398aaedb3f826f5294b9e9713429ec
kamilah <milahkesi@gmail.com>
2014-10-26 18:53:41 +0000

    associate products with users

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,5 +11,8 @@ class Product < ActiveRecord::Base
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates_attachment_presence :image
+  
+    belongs_to :user
+
 
 end
