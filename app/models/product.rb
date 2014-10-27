d8604c6e0650845d955daaeecb388f450c96d6f1
kamilah <milahkesi@gmail.com>
2014-10-27 17:07:24 +0000

    redesign seller page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
   if Rails.env.development?
-   has_attached_file :image, :styles => { :medium => "300x", :thumb => "200x200>" }, :default_url => "default.jpg"
+   has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "default.jpg"
   else
-   has_attached_file :image, :styles => { :medium => "300x", :thumb => "200x200>" }, :default_url => "default.jpg",
+   has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "default.jpg",
                      :storage => :dropbox,
                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                      :path => ":style/:id_:filename"
